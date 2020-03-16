3bc7c76bb1774fddb9eb18e76877871286b1f6a0
Anthony <hanthony1999@yahoo.com>
2020-03-16 15:46:44 -0500

    updated files

--- Project0/Product.cs
+++ Project0/Product.cs
@@ -10,19 +10,50 @@ namespace Project0
 {
     class Product
     {
+        // fields
+        public string itemName { get; set; }
+        public int itemAmmount { get; set; }
+        public int itemID { get; set; }
 
-        public string itemName;
-        public int itemID;
 
-        public Product(string itemName, int itemID)
+        // Construtor
+        public Product(string itemName, int itemID, int itemAmmount)
         {
             this.itemName = itemName;
             this.itemID = itemID;
+            this.itemAmmount = itemAmmount;
         }
 
+
+        // used to return user information
         public override string ToString()
         {        
             return $"Product Info : {itemName} : ID = {itemID}";
+        }
+
+
+        // method used to get an Item name to add to inventory at the store
+        private string AddItemName()
+        {
+            Console.WriteLine("Please Enter An Item Name");
+            string itemName = Console.ReadLine();
+            return itemName;
+        }
+
+
+        // method used to add item count from item in the inventory
+        private int  AddItemAmmount()
+        {
+            Console.WriteLine("Please Enter An Item Name");
+            int itemName = Int32.Parse(Console.ReadLine());
+            return itemName;
+        }
+
+
+        // method to add an ID to the item, ID will be provided by the database 
+        private int AddItemID()
+        {
+            return itemID;
         }
     }
 }
---
+++ Project0/Serialization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Project0.Library
+{
+    class Serialization
+    {
+        string UserFilePath = "../../../User_Data.json";
+        string StoreFilePath = "../../../Store_Data.json";
+        string ProductFilePath = "../../../Product_Data.json";
+
+        List<Person> data = null;
+
+        private async static Task<string> ReadFromJSON(string filePath)
+        {
+            using var sr = new StreamReader(filePath);
+            Task<string> textTask = sr.ReadToEndAsync();
+            string text = await textTask;
+            return text;
+
+        }
+
+        private async static Task WriteFileJSON(string text,  string path)
+        {
+            FileStream file = null;
+            file = new FileStream(path, FileMode.Create);
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            await file.WriteAsync(data);
+        }
+
+        static string ConvertUserToJSON(List<Person> data)
+        {
+            return JsonSerializer.Serialize(data);
+        }
+
+        static string ConvertStoreToJSON(List<Store> data)
+        {
+            return JsonSerializer.Serialize(data);
+        }
+    }
+}
