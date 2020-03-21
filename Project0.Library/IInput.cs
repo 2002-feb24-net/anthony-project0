837ebdfc3f01c613b658e0d8cb460f5482ffcc42
Anthony <hanthony1999@yahoo.com>
2020-03-21 11:39:30 -0500

    minor changes

---
+++ Project0.Library/IInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library
+{
+    interface IInput
+    {
+        public interface IInputInfo()
+        {
+            string Input();
+        }
+    }
+}
