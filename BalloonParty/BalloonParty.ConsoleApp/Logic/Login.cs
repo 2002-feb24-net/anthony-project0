6a6bb71b58f2d384793adac39b925bf51d0f84b2
Anthony <hanthony1999@yahoo.com>
2020-04-01 10:17:10 -0500

    updated validation on user selection screen

--- BalloonParty/BalloonParty.ConsoleApp/Logic/Login.cs
+++ BalloonParty/BalloonParty.ConsoleApp/Logic/Login.cs
@@ -8,8 +8,19 @@ namespace BalloonParty.ConsoleApp.Logic
         // Main Login Screen
         public static void mainLogin()
         {
-            System.Console.WriteLine("Please select if you are a customer, a store, or an admin\n1: Customer\n2: Store\n3: Admin");
-            int input = int.Parse(Console.ReadLine());
+            bool status = true;
+            int newInput;
+            do{
+                System.Console.WriteLine("Please select if you are a customer, a store, or an admin\n1: Customer\n2: Store\n3: Admin");
+                string input = Console.ReadLine();
+                if(input == "1" || input == "2" || input == "3")
+                {
+                    newInput  = int.Parse(input);
+                    status = false;
+                }
+                
+            }while(status);
+            
             System.Console.WriteLine("\nPLEASE LOGIN");
             System.Console.WriteLine("Please Enter Your Login ID");
             string email = Console.ReadLine();
@@ -19,19 +30,19 @@ namespace BalloonParty.ConsoleApp.Logic
             {
                 try
                 {
-                    if(input == 1)
+                    if(newInput == 1)
                     {
                         var users = context.Customer.First(c=> c.EmailAddress == email && c.CustomerPw == pw);
                         System.Console.Clear();
                         userInterface.userMainMenu();
                     }
-                    if(input == 2)
+                    if(newInput == 2)
                     {
                         var storeUser = context.Store.First(s=> s.StoreUsername == email && s.StorePw == pw);
                         System.Console.Clear();
                         storeInterface.storeMainMenu();
                     }
-                    if(input == 3)
+                    if(newInput == 3)
                     {
                         var rootuser = context.RootUser.First(r => r.RootName == email && r.RootPw == pw);     
                         System.Console.Clear();
@@ -47,9 +58,6 @@ namespace BalloonParty.ConsoleApp.Logic
                     mainLogin();
                 }
             }
-            
-
-
         }
     }
 }
